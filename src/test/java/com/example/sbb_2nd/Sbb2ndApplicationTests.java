8ecbb306ce24a802b7c159c73e9b8699a2bc7909
chener <49267211+chanosong@users.noreply.github.com>
2023-03-28 15:26:49 +0900

    feat: Intialize springboot proj

---
+++ src/test/java/com/example/sbb_2nd/Sbb2ndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb_2nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb2ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
